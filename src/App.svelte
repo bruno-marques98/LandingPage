afb2b226d0d2f4e9953e8cc16270dfdf627e8522
bruno-marques98 <bruno98silva@hotmail.com>
2022-04-19 10:48:27 +0100

    Init

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import MaterialButton from "./MaterialButton.svelte";
+</script>
+
+<MaterialButton id="one" text="first button" onClickFunction={() => {
+	console.log("clicked first button");
+}} />
+<MaterialButton id="two" text="second button" onClickFunction={() => {
+	console.log("clicked second button");
+}} />
+
+<style global lang="scss">
+@use "@material/button/styles";
+</style>
