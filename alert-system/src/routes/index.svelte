bdbd38809b09026d97a17363ccb00934b77db589
bruno-marques98 <bruno98silva@hotmail.com>
2022-04-20 16:29:38 +0100

    change info in index

--- alert-system/src/routes/index.svelte
+++ alert-system/src/routes/index.svelte
@@ -29,80 +29,105 @@
 	}
 	import './index.scss';
 	import Ref from '../components/refresh.svelte';
-
 </script>
 
 
 
 
-{#if isAnyServiceStopped == true}
-	<div style="padding: 20px; background-color: #f44336; color: white;">
-		<strong>Atenção!</strong> Existem serviços parados
-		<span
-			style="margin-left: 15px;
-		color: white;
-		font-weight: bold;
-		float: right;
-		font-size: 22px;
-		line-height: 20px;
-		cursor: pointer;
-		transition: 0.3s;"
-			onclick="this.parentElement.style.display='none';">&times;</span
-		>
-	</div>
-{/if}
-
-<div style="display: flex; justify-content: center;">
-	<div
-		style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-		text-align: center; padding: 20px 60px; margin: 20px"
-	>
-		<h1 class="text-4xl text-begin uppercase" style="color:red">IS Retail</h1>
-		<p class="text-2xl text-begin uppercase">Mensagens</p>
-		<ol style="margin: 10px">
-			{#await promise}
-				<p>Loading...</p>
-			{:then status}
-				{#each status as stat}
-					<li
-						style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px;  color: red"
-					>
+{#await promise}
+	<p>Loading...</p>
+{:then status}
+	{#if isAnyServiceStopped == true}
+		<div style="padding: 20px; background-color: #f44336; color: white;">
+			<strong>Atenção!</strong> Existem serviços parados
+			<span
+				style="margin-left: 15px; color: white; font-weight: bold; float: right; font-size: 22px; line-height: 20px; cursor: pointer; transition: 0.3s;"
+				onclick="this.parentElement.style.display='none';">&times;</span>
+		</div>
+	{/if}
+	{#each status as stat}
+			{#if stat.Info == 'Services inactive'}
+				{#if stat.Value > 0}
+				<div style="padding: 20px; background-color: #f44336; color: white;">
+					<strong>Atenção!</strong> {stat.Info} - {stat.Value}
+					<span
+						style="margin-left: 15px; color: white; font-weight: bold; float: right; font-size: 22px; line-height: 20px; cursor: pointer; transition: 0.3s;"
+						onclick="this.parentElement.style.display='none';">&times;</span>
+				</div>
+				{/if}
+			{:else if stat.Info == 'Messages in error'}
+				{#if stat.Value > 0}
+				<div style="padding: 20px; background-color: #f44336; color: white;">
+					<strong>Atenção!</strong> {stat.Info} - {stat.Value}
+					<span
+						style="margin-left: 15px; color: white; font-weight: bold; float: right; font-size: 22px; line-height: 20px; cursor: pointer; transition: 0.3s;"
+						onclick="this.parentElement.style.display='none';">&times;</span>
+				</div>
+				{/if}
+			{:else if stat.Info == 'Jobs in error'}
+				{#if stat.Value > 0}
+				<div style="padding: 20px; background-color: #f44336; color: white;">
+					<strong>Atenção!</strong> {stat.Info} - {stat.Value}
+					<span
+						style="margin-left: 15px; color: white; font-weight: bold; float: right; font-size: 22px; line-height: 20px; cursor: pointer; transition: 0.3s;"
+						onclick="this.parentElement.style.display='none';">&times;</span>
+				</div>
+				{/if}
+			{/if}
+	{/each}
+	<div style="display: flex; justify-content: center;">
+		<div
+			style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); text-align: center; padding: 20px 60px; margin: 20px">
+			<h1 class="text-4xl text-begin uppercase" style="color:red">IS Retail</h1>
+			<p class="text-2xl text-begin uppercase">Mensagens</p>
+			<ol style="margin: 10px">
+		{#each status as stat}
+			{#if stat.Info == 'Services inactive'}
+				{#if stat.Value > 0}
+					<li style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE;	padding: 12px;  color: red">
 						{stat.Info} : {stat.Value}
 					</li>
-					<p hidden>
-						{#if stat.Info == 'Services inactive'}
-							{#if stat.Value > 0}
-								{setServicesTrue()}
-							{/if}
-						{/if}
-					</p>
+				{/if}
+			{:else if stat.Info == 'Messages in error'}
+				{#if stat.Value > 0}
+					<li style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red" >
+						{stat.Info} : {stat.Value}
+					</li>
+				{/if}
+			{:else if stat.Info == 'Jobs in error'}
+				{#if stat.Value > 0}
+				<li style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red" >
+						{stat.Info} : {stat.Value}
+					</li>
+				{/if}
+			{/if}
+			
+			<p hidden>
+				{#if stat.Info == 'Services inactive'}
+					{#if stat.Value > 0}
+						{setServicesTrue()}
+					{/if}
+				{/if}
+			</p>
+		{/each}
+			</ol>
+			<p class="text-2xl text-begin uppercase; margin: 10px">Numeradores a acabar</p>
+			<ol style="margin: 10px">
+				{#each $numeradores as num}
+				<li style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red" >
+						{num.name} : {num.resto}
+					</li>
 				{/each}
-			{:catch error}
+			</ol>
+		</div>
+			</div>
+				{:catch error}
 				<p style="color: red">{error.message}</p>
 			{/await}
-		</ol>
-		<p class="text-2xl text-begin uppercase; margin: 10px">Numeradores a acabar</p>
-		<ol style="margin: 10px">
-			{#each $numeradores as num}
-				<li
-					style="border: 1px solid #ddd;
-			margin-top: -1px; /* Prevent double borders */
-			background-color: #C9C0BE;
-			padding: 12px;  color: red"
-				>
-					{num.name} : {num.resto}
-				</li>
-			{/each}
-		</ol>
-	</div>
-
+		
+<div>
 	<div
-		style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-	text-align: center; padding: 20px 60px; margin: 20px"
-	>
+		style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);text-align: center; padding: 20px 60px; margin: 20px">
 		<h1 class="text-4xl text-begin uppercase" style="color:red">AGRO</h1>
 		<p class="text-2xl text-begin uppercase">Mensagens</p>
 		<ol style="margin: 10px">
@@ -146,8 +171,7 @@
 	</div>
 	<div
 		style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-		text-align: center; padding: 20px 60px; margin: 20px"
-	>
+		text-align: center; padding: 20px 60px; margin: 20px">
 		<h1 class="text-4xl text-begin uppercase" style="color:red">STAPLES</h1>
 		<p class="text-2xl text-begin uppercase">Mensagens</p>
 		<ol style="margin: 10px">
@@ -191,8 +215,7 @@
 	</div>
 	<div
 		style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-		text-align: center; padding: 20px 60px; margin: 20px"
-	>
+		text-align: center; padding: 20px 60px; margin: 20px">
 		<h1 class="text-4xl text-begin uppercase" style="color:red">DAUFOOD</h1>
 		<p class="text-2xl text-begin uppercase">Mensagens</p>
 		<ol style="margin: 10px">
@@ -225,9 +248,9 @@
 			{#each $numeradores as num}
 				<li
 					style="border: 1px solid #ddd;
-			margin-top: -1px; /* Prevent double borders */
-			background-color: #C9C0BE;
-			padding: 12px;  color: red"
+						margin-top: -1px; /* Prevent double borders */
+						background-color: #C9C0BE;
+						padding: 12px;  color: red"
 				>
 					{num.name} : {num.resto}
 				</li>
@@ -236,321 +259,4 @@
 	</div>
 </div>
 
-<Ref/>
-
-<!--
-<div style="padding: 20px; background-color: #f44336; color: white;">
-	<strong>Atenção!</strong> Existem serviços parados
-	<span style="margin-left: 15px;
-	color: white;
-	font-weight: bold;
-	float: right;
-	font-size: 22px;
-	line-height: 20px;
-	cursor: pointer;
-	transition: 0.3s;" onclick="this.parentElement.style.display='none';">&times;</span> 
-  </div>
-<h1 class="text-4xl text-center my-8 uppercase">Sistema de alertas</h1>
-<div style="display: flex; justify-content: center;">
-	<div style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-	text-align: center; padding: 0px 60px; margin: 20px">
-		<h1 class="text-4xl text-begin uppercase"  style="color:red">IS Retail</h1>
-		<p class="text-2xl text-begin uppercase">Serviços</p>
-		<div style="display: grid; align-items: center;">
-			<Section style="margin: 0px">
-				<p>Serviço Batch 1</p>
-				<IconButton class="material-icons" >
-					<Icon component={Svg} viewBox="0 0 24 24">
-						<path fill="red" d={mdiCircle} />
-					</Icon>
-				</IconButton>
-			</Section>
-			<Section style="margin: 0px">
-				<p>Serviço Batch 2</p>
-				<IconButton class="material-icons" size="normal" >
-					<Icon component={Svg} viewBox="0 0 24 24" >
-						<path fill="green" d={mdiCircle} />
-					</Icon>
-				</IconButton>
-			</Section>
-			<Section style="margin: 0px">
-				<p>Serviço Batch 3</p>
-				<IconButton class="material-icons" size="normal" >
-					<Icon component={Svg} viewBox="0 0 24 24" >
-						<path fill="green" d={mdiCircle} />
-					</Icon>
-				</IconButton>
-			</Section>
-		</div>
-		<p class="text-2xl text-begin uppercase">Numeradores</p>
-		<div style="display: flex; align-items: center;">
-			<Section style="margin: 0px">
-				<ui  style="list-style-type: none;">
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE; /* Grey background on top of the list items */
-					padding: 12px;">
-						Numerador XPTO: 759
-					</li>
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px;  color: red">
-						Numerador XPTO2: 10
-					</li>
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px; color: red">
-						Numerador XPTO3: 0
-					</li>
-				</ui>
-			</Section>
-		</div>
-		<p class="text-2xl text-begin uppercase">Erros</p>
-		<div style="display: flex; align-items: center;">
-			<Section style="margin: 0px">
-				<ui  style="list-style-type: none; width: 100%;">
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px;">
-						Mensagens: 759
-					</li>
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px;">
-						Trabalhos: 10
-					</li>
-				</ui>
-			</Section>
-		</div>
-	</div>
-	<div style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-	text-align: center; padding: 0px 60px; margin: 20px">
-		<h1 class="text-4xl text-begin uppercase"  style="color:red">Agros</h1>
-		<p class="text-2xl text-begin uppercase">Serviços</p>
-		<div style="display: grid; align-items: center;">
-			<Section style="margin: 0px">
-				<p>Serviço Batch 1</p>
-				<IconButton class="material-icons" >
-					<Icon component={Svg} viewBox="0 0 24 24">
-						<path fill="red" d={mdiCircle} />
-					</Icon>
-				</IconButton>
-			</Section>
-			<Section style="margin: 0px">
-				<p>Serviço Batch 2</p>
-				<IconButton class="material-icons" size="normal" >
-					<Icon component={Svg} viewBox="0 0 24 24" >
-						<path fill="green" d={mdiCircle} />
-					</Icon>
-				</IconButton>
-			</Section>
-			<Section style="margin: 0px">
-				<p>Serviço Batch 3</p>
-				<IconButton class="material-icons" size="normal" >
-					<Icon component={Svg} viewBox="0 0 24 24" >
-						<path fill="green" d={mdiCircle} />
-					</Icon>
-				</IconButton>
-			</Section>
-		</div>
-		<p class="text-2xl text-begin uppercase">Numeradores</p>
-		<div style="display: flex; align-items: center;">
-			<Section style="margin: 0px">
-				<ui  style="list-style-type: none;">
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE; /* Grey background on top of the list items */
-					padding: 12px;">
-						Numerador XPTO: 759
-					</li>
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px;  color: red">
-						Numerador XPTO2: 10
-					</li>
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px; color: red">
-						Numerador XPTO3: 0
-					</li>
-				</ui>
-			</Section>
-		</div>
-		<p class="text-2xl text-begin uppercase">Erros</p>
-		<div style="display: flex; align-items: center;">
-			<Section style="margin: 0px">
-				<ui  style="list-style-type: none; width: 100%;">
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px;">
-						Mensagens: 759
-					</li>
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px;">
-						Trabalhos: 10
-					</li>
-				</ui>
-			</Section>
-		</div>
-	</div>
-	<div style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-	text-align: center; padding: 0px 60px; margin: 20px">
-		<h1 class="text-4xl text-begin uppercase"  style="color:red">Daufood</h1>
-		<p class="text-2xl text-begin uppercase">Serviços</p>
-		<div style="display: grid; align-items: center;">
-			<Section style="margin: 0px">
-				<p>Serviço Batch 1</p>
-				<IconButton class="material-icons" >
-					<Icon component={Svg} viewBox="0 0 24 24">
-						<path fill="red" d={mdiCircle} />
-					</Icon>
-				</IconButton>
-			</Section>
-			<Section style="margin: 0px">
-				<p>Serviço Batch 2</p>
-				<IconButton class="material-icons" size="normal" >
-					<Icon component={Svg} viewBox="0 0 24 24" >
-						<path fill="green" d={mdiCircle} />
-					</Icon>
-				</IconButton>
-			</Section>
-			<Section style="margin: 0px">
-				<p>Serviço Batch 3</p>
-				<IconButton class="material-icons" size="normal" >
-					<Icon component={Svg} viewBox="0 0 24 24" >
-						<path fill="green" d={mdiCircle} />
-					</Icon>
-				</IconButton>
-			</Section>
-		</div>
-		<p class="text-2xl text-begin uppercase">Numeradores</p>
-		<div style="display: flex; align-items: center;">
-			<Section style="margin: 0px">
-				<ui  style="list-style-type: none;">
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE; /* Grey background on top of the list items */
-					padding: 12px;">
-						Numerador XPTO: 759
-					</li>
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px;  color: red">
-						Numerador XPTO2: 10
-					</li>
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px; color: red">
-						Numerador XPTO3: 0
-					</li>
-				</ui>
-			</Section>
-		</div>
-		<p class="text-2xl text-begin uppercase">Erros</p>
-		<div style="display: flex; align-items: center;">
-			<Section style="margin: 0px">
-				<ui  style="list-style-type: none; width: 100%;">
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px;">
-						Mensagens: 759
-					</li>
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px;">
-						Trabalhos: 10
-					</li>
-				</ui>
-			</Section>
-		</div>
-	</div>
-	<div style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-	text-align: center; padding: 0px 60px; margin: 20px">
-		<h1 class="text-4xl text-begin uppercase"  style="color:red">Staples</h1>
-		<p class="text-2xl text-begin uppercase">Serviços</p>
-		<div style="display: grid; align-items: center;">
-			<Section style="margin: 0px">
-				<p>Serviço Batch 1</p>
-				<IconButton class="material-icons" >
-					<Icon component={Svg} viewBox="0 0 24 24">
-						<path fill="red" d={mdiCircle} />
-					</Icon>
-				</IconButton>
-			</Section>
-			<Section style="margin: 0px">
-				<p>Serviço Batch 2</p>
-				<IconButton class="material-icons" size="normal" >
-					<Icon component={Svg} viewBox="0 0 24 24" >
-						<path fill="green" d={mdiCircle} />
-					</Icon>
-				</IconButton>
-			</Section>
-			<Section style="margin: 0px">
-				<p>Serviço Batch 3</p>
-				<IconButton class="material-icons" size="normal" >
-					<Icon component={Svg} viewBox="0 0 24 24" >
-						<path fill="green" d={mdiCircle} />
-					</Icon>
-				</IconButton>
-			</Section>
-		</div>
-		<p class="text-2xl text-begin uppercase">Numeradores</p>
-		<div style="display: flex; align-items: center;">
-			<Section style="margin: 0px">
-				<ui  style="list-style-type: none;">
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE; /* Grey background on top of the list items */
-					padding: 12px;">
-						Numerador XPTO: 759
-					</li>
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px;  color: red">
-						Numerador XPTO2: 10
-					</li>
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px; color: red">
-						Numerador XPTO3: 0
-					</li>
-				</ui>
-			</Section>
-		</div>
-		<p class="text-2xl text-begin uppercase">Erros</p>
-		<div style="display: flex; align-items: center;">
-			<Section style="margin: 0px">
-				<ui  style="list-style-type: none; width: 100%;">
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px;">
-						Mensagens: 759
-					</li>
-					<li style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px;">
-						Trabalhos: 10
-					</li>
-				</ui>
-			</Section>
-		</div>
-	</div>
-</div>
--->
+<Ref />
