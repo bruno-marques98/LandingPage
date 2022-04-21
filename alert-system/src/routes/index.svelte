c1873a17f1af32471851bb915f8aed1bcbe1d185
bruno-marques98 <bruno98silva@hotmail.com>
2022-04-21 12:35:12 +0100

    Change alert bar and added more clients

--- alert-system/src/routes/index.svelte
+++ alert-system/src/routes/index.svelte
@@ -20,232 +20,373 @@
 	}
 	import './index.scss';
 	import Ref from '../components/refresh.svelte';
+
+	let errorMessage = 'Hello';
+	function setMessage(message) {
+		errorMessage = message;
+	}
 </script>
 
-
-{#await promise}
-	<p>Loading...</p>
-{:then status}
-	{#if isAnyServiceStopped == true}
-		<div style="padding: 20px; background-color: #f44336; color: white;">
-			<strong>Atenção!</strong> Existem serviços parados
-			<span
-				style="margin-left: 15px; color: white; font-weight: bold; float: right; font-size: 22px; line-height: 20px; cursor: pointer; transition: 0.3s;"
-				onclick="this.parentElement.style.display='none';">&times;</span>
-		</div>
-	{/if}
-	{#each status as stat}
-			{#if stat.Info == 'Services inactive'}
-				{#if stat.Value > 0}
-				<div style="padding: 20px; background-color: #f44336; color: white;">
-					<strong>Atenção!</strong> {stat.Info} - {stat.Value}
-					<span
-						style="margin-left: 15px; color: white; font-weight: bold; float: right; font-size: 22px; line-height: 20px; cursor: pointer; transition: 0.3s;"
-						onclick="this.parentElement.style.display='none';">&times;</span>
-				</div>
-				{/if}
-			{:else if stat.Info == 'Messages in error'}
-				{#if stat.Value > 0}
-				<div style="padding: 20px; background-color: #f44336; color: white;">
-					<strong>Atenção!</strong> {stat.Info} - {stat.Value}
-					<span
-						style="margin-left: 15px; color: white; font-weight: bold; float: right; font-size: 22px; line-height: 20px; cursor: pointer; transition: 0.3s;"
-						onclick="this.parentElement.style.display='none';">&times;</span>
-				</div>
-				{/if}
-			{:else if stat.Info == 'Jobs in error'}
-				{#if stat.Value > 0}
-				<div style="padding: 20px; background-color: #f44336; color: white;">
-					<strong>Atenção!</strong> {stat.Info} - {stat.Value}
-					<span
-						style="margin-left: 15px; color: white; font-weight: bold; float: right; font-size: 22px; line-height: 20px; cursor: pointer; transition: 0.3s;"
-						onclick="this.parentElement.style.display='none';">&times;</span>
-				</div>
-				{/if}
-			{/if}
-	{/each}
-	<div style="display: flex; justify-content: center;">
-		<div
-			style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); text-align: center; padding: 20px 60px; margin: 20px">
-			<h1 class="text-4xl text-begin uppercase" style="color:red">IS Retail</h1>
-			<p class="text-2xl text-begin uppercase">Mensagens</p>
-			<ol style="margin: 10px">
+{#if isAnyServiceStopped == true}
+	<div style="padding: 20px; background-color: #f44336; color: white;">
+		<strong>Atenção!</strong>
+		{errorMessage}
+		<span
+			style="margin-left: 15px; color: white; font-weight: bold; float: right; font-size: 22px; line-height: 20px; cursor: pointer; transition: 0.3s;"
+			onclick="this.parentElement.style.display='none';">&times;</span
+		>
+	</div>
+{/if}
+<div style="display:flex; justify-content: center">
+	{#await promise}
+		<p>Loading...</p>
+	{:then status}
 		{#each status as stat}
 			{#if stat.Info == 'Services inactive'}
 				{#if stat.Value > 0}
-					<li style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE;	padding: 12px;  color: red">
-						{stat.Info} : {stat.Value}
-					</li>
+					<p hidden>
+						{setServicesTrue()}
+						{setMessage('There are Services Inactive!')}
+					</p>
 				{/if}
 			{:else if stat.Info == 'Messages in error'}
 				{#if stat.Value > 0}
-					<li style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red" >
-						{stat.Info} : {stat.Value}
-					</li>
+					<p hidden>
+						{setServicesTrue()}
+						{setMessage('There are Messages in error!')}
+					</p>
 				{/if}
 			{:else if stat.Info == 'Jobs in error'}
 				{#if stat.Value > 0}
-				<li style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red" >
-						{stat.Info} : {stat.Value}
-					</li>
+					<p hidden>
+						{setServicesTrue()}
+						{setMessage('There are Jobs in error!')}
+					</p>
 				{/if}
 			{/if}
-			
-			<p hidden>
-				{#if stat.Info == 'Services inactive'}
-					{#if stat.Value > 0}
-						{setServicesTrue()}
-					{/if}
-				{/if}
-			</p>
 		{/each}
-			</ol>
-			<p class="text-2xl text-begin uppercase; margin: 10px">Numeradores a acabar</p>
-			<ol style="margin: 10px">
-				{#each $numeradores as num}
-				<li style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red" >
-						{num.name} : {num.resto}
-					</li>
-				{/each}
-			</ol>
-		</div>
+		<div style="display: flex; justify-content: center;">
+			<div
+				style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); text-align: center; padding: 20px 60px; margin: 20px"
+			>
+				<h1 class="text-4xl text-begin uppercase" style="color:red">IS Retail</h1>
+				<p class="text-2xl text-begin uppercase">Mensagens</p>
+				<ol style="margin: 10px">
+					{#each status as stat}
+						{#if stat.Info == 'Services inactive'}
+							{#if stat.Value > 0}
+								<li
+									style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE;	padding: 12px;  color: red"
+								>
+									{stat.Info} : {stat.Value}
+								</li>
+							{/if}
+						{:else if stat.Info == 'Messages in error'}
+							{#if stat.Value > 0}
+								<li
+									style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red"
+								>
+									{stat.Info} : {stat.Value}
+								</li>
+							{/if}
+						{:else if stat.Info == 'Jobs in error'}
+							{#if stat.Value > 0}
+								<li
+									style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red"
+								>
+									{stat.Info} : {stat.Value}
+								</li>
+							{/if}
+						{/if}
+
+						<p hidden>
+							{#if stat.Info == 'Services inactive'}
+								{#if stat.Value > 0}
+									{setServicesTrue()}
+								{/if}
+							{/if}
+						</p>
+					{/each}
+				</ol>
+				{#if $numeradores.length > 0}
+					<p class="text-2xl text-begin uppercase; margin: 10px">Numeradores a acabar</p>
+				{/if}
+				<ol style="margin: 10px">
+					{#each $numeradores as num}
+						<li
+							style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red"
+						>
+							{num.name} : {num.resto}
+						</li>
+					{/each}
+				</ol>
 			</div>
-				{:catch error}
-				<p style="color: red">{error.message}</p>
-			{/await}
-		
-<div>
-	<div
-		style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);text-align: center; padding: 20px 60px; margin: 20px">
-		<h1 class="text-4xl text-begin uppercase" style="color:red">AGRO</h1>
-		<p class="text-2xl text-begin uppercase">Mensagens</p>
-		<ol style="margin: 10px">
-			{#await promise}
-				<p>Loading...</p>
-			{:then status}
-				{#each status as stat}
-					<li
-						style="border: 1px solid #ddd;
-				margin-top: -1px; /* Prevent double borders */
-				background-color: #C9C0BE;
-				padding: 12px;  color: red"
-					>
-						{stat.Info} : {stat.Value}
-					</li>
+		</div>
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
+	{#await promise}
+		<p>Loading...</p>
+	{:then status}
+		{#each status as stat}
+			{#if stat.Info == 'Services inactive'}
+				{#if stat.Value > 0}
 					<p hidden>
+						{setServicesTrue()}
+						{setMessage('There are Services Inactive!')}
+					</p>
+				{/if}
+			{:else if stat.Info == 'Messages in error'}
+				{#if stat.Value > 0}
+					<p hidden>
+						{setServicesTrue()}
+						{setMessage('There are Messages in error!')}
+					</p>
+				{/if}
+			{:else if stat.Info == 'Jobs in error'}
+				{#if stat.Value > 0}
+					<p hidden>
+						{setServicesTrue()}
+						{setMessage('There are Jobs in error!')}
+					</p>
+				{/if}
+			{/if}
+		{/each}
+		<div style="display: flex; justify-content: center;">
+			<div
+				style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); text-align: center; padding: 20px 60px; margin: 20px"
+			>
+				<h1 class="text-4xl text-begin uppercase" style="color:red">Staples</h1>
+				<p class="text-2xl text-begin uppercase">Mensagens</p>
+				<ol style="margin: 10px">
+					{#each status as stat}
 						{#if stat.Info == 'Services inactive'}
 							{#if stat.Value > 0}
-								{setServicesTrue()}
+								<li
+									style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE;	padding: 12px;  color: red"
+								>
+									{stat.Info} : {stat.Value}
+								</li>
+							{/if}
+						{:else if stat.Info == 'Messages in error'}
+							{#if stat.Value > 0}
+								<li
+									style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red"
+								>
+									{stat.Info} : {stat.Value}
+								</li>
+							{/if}
+						{:else if stat.Info == 'Jobs in error'}
+							{#if stat.Value > 0}
+								<li
+									style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red"
+								>
+									{stat.Info} : {stat.Value}
+								</li>
 							{/if}
 						{/if}
-					</p>
-				{/each}
-			{:catch error}
-				<p style="color: red">{error.message}</p>
-			{/await}
-		</ol>
-		<p class="text-2xl text-begin uppercase; margin: 10px">Numeradores a acabar</p>
-		<ol style="margin: 10px">
-			{#each $numeradores as num}
-				<li
-					style="border: 1px solid #ddd;
-		margin-top: -1px; /* Prevent double borders */
-		background-color: #C9C0BE;
-		padding: 12px;  color: red"
-				>
-					{num.name} : {num.resto}
-				</li>
-			{/each}
-		</ol>
-	</div>
-	<div
-		style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-		text-align: center; padding: 20px 60px; margin: 20px">
-		<h1 class="text-4xl text-begin uppercase" style="color:red">STAPLES</h1>
-		<p class="text-2xl text-begin uppercase">Mensagens</p>
-		<ol style="margin: 10px">
-			{#await promise}
-				<p>Loading...</p>
-			{:then status}
-				{#each status as stat}
-					<li
-						style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px;  color: red"
-					>
-						{stat.Info} : {stat.Value}
-					</li>
+
+						<p hidden>
+							{#if stat.Info == 'Services inactive'}
+								{#if stat.Value > 0}
+									{setServicesTrue()}
+								{/if}
+							{/if}
+						</p>
+					{/each}
+				</ol>
+				{#if $numeradores.length > 0}
+					<p class="text-2xl text-begin uppercase; margin: 10px">Numeradores a acabar</p>
+				{/if}
+				<ol style="margin: 10px">
+					{#each $numeradores as num}
+						<li
+							style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red"
+						>
+							{num.name} : {num.resto}
+						</li>
+					{/each}
+				</ol>
+			</div>
+		</div>
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
+	{#await promise}
+		<p>Loading...</p>
+	{:then status}
+		{#each status as stat}
+			{#if stat.Info == 'Services inactive'}
+				{#if stat.Value > 0}
 					<p hidden>
+						{setServicesTrue()}
+						{setMessage('There are Services Inactive!')}
+					</p>
+				{/if}
+			{:else if stat.Info == 'Messages in error'}
+				{#if stat.Value > 0}
+					<p hidden>
+						{setServicesTrue()}
+						{setMessage('There are Messages in error!')}
+					</p>
+				{/if}
+			{:else if stat.Info == 'Jobs in error'}
+				{#if stat.Value > 0}
+					<p hidden>
+						{setServicesTrue()}
+						{setMessage('There are Jobs in error!')}
+					</p>
+				{/if}
+			{/if}
+		{/each}
+		<div style="display: flex; justify-content: center;">
+			<div
+				style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); text-align: center; padding: 20px 60px; margin: 20px"
+			>
+				<h1 class="text-4xl text-begin uppercase" style="color:red">Agro</h1>
+				<p class="text-2xl text-begin uppercase">Mensagens</p>
+				<ol style="margin: 10px">
+					{#each status as stat}
 						{#if stat.Info == 'Services inactive'}
 							{#if stat.Value > 0}
-								{setServicesTrue()}
+								<li
+									style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE;	padding: 12px;  color: red"
+								>
+									{stat.Info} : {stat.Value}
+								</li>
+							{/if}
+						{:else if stat.Info == 'Messages in error'}
+							{#if stat.Value > 0}
+								<li
+									style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red"
+								>
+									{stat.Info} : {stat.Value}
+								</li>
+							{/if}
+						{:else if stat.Info == 'Jobs in error'}
+							{#if stat.Value > 0}
+								<li
+									style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red"
+								>
+									{stat.Info} : {stat.Value}
+								</li>
 							{/if}
 						{/if}
-					</p>
-				{/each}
-			{:catch error}
-				<p style="color: red">{error.message}</p>
-			{/await}
-		</ol>
-		<p class="text-2xl text-begin uppercase; margin: 10px">Numeradores a acabar</p>
-		<ol style="margin: 10px">
-			{#each $numeradores as num}
-				<li
-					style="border: 1px solid #ddd;
-			margin-top: -1px; /* Prevent double borders */
-			background-color: #C9C0BE;
-			padding: 12px;  color: red"
-				>
-					{num.name} : {num.resto}
-				</li>
-			{/each}
-		</ol>
-	</div>
-	<div
-		style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-		text-align: center; padding: 20px 60px; margin: 20px">
-		<h1 class="text-4xl text-begin uppercase" style="color:red">DAUFOOD</h1>
-		<p class="text-2xl text-begin uppercase">Mensagens</p>
-		<ol style="margin: 10px">
-			{#await promise}
-				<p>Loading...</p>
-			{:then status}
-				{#each status as stat}
-					<li
-						style="border: 1px solid #ddd;
-					margin-top: -1px; /* Prevent double borders */
-					background-color: #C9C0BE;
-					padding: 12px;  color: red"
-					>
-						{stat.Info} : {stat.Value}
-					</li>
+
+						<p hidden>
+							{#if stat.Info == 'Services inactive'}
+								{#if stat.Value > 0}
+									{setServicesTrue()}
+								{/if}
+							{/if}
+						</p>
+					{/each}
+				</ol>
+				{#if $numeradores.length > 0}
+					<p class="text-2xl text-begin uppercase; margin: 10px">Numeradores a acabar</p>
+				{/if}
+				<ol style="margin: 10px">
+					{#each $numeradores as num}
+						<li
+							style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red"
+						>
+							{num.name} : {num.resto}
+						</li>
+					{/each}
+				</ol>
+			</div>
+		</div>
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
+
+	{#await promise}
+		<p>Loading...</p>
+	{:then status}
+		{#each status as stat}
+			{#if stat.Info == 'Services inactive'}
+				{#if stat.Value > 0}
 					<p hidden>
+						{setServicesTrue()}
+						{setMessage('There are Services Inactive!')}
+					</p>
+				{/if}
+			{:else if stat.Info == 'Messages in error'}
+				{#if stat.Value > 0}
+					<p hidden>
+						{setServicesTrue()}
+						{setMessage('There are Messages in error!')}
+					</p>
+				{/if}
+			{:else if stat.Info == 'Jobs in error'}
+				{#if stat.Value > 0}
+					<p hidden>
+						{setServicesTrue()}
+						{setMessage('There are Jobs in error!')}
+					</p>
+				{/if}
+			{/if}
+		{/each}
+		<div style="display: flex; justify-content: center;">
+			<div
+				style="display: grid;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); text-align: center; padding: 20px 60px; margin: 20px"
+			>
+				<h1 class="text-4xl text-begin uppercase" style="color:red">Daufood</h1>
+				<p class="text-2xl text-begin uppercase">Mensagens</p>
+				<ol style="margin: 10px">
+					{#each status as stat}
 						{#if stat.Info == 'Services inactive'}
 							{#if stat.Value > 0}
-								{setServicesTrue()}
+								<li
+									style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE;	padding: 12px;  color: red"
+								>
+									{stat.Info} : {stat.Value}
+								</li>
+							{/if}
+						{:else if stat.Info == 'Messages in error'}
+							{#if stat.Value > 0}
+								<li
+									style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red"
+								>
+									{stat.Info} : {stat.Value}
+								</li>
+							{/if}
+						{:else if stat.Info == 'Jobs in error'}
+							{#if stat.Value > 0}
+								<li
+									style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red"
+								>
+									{stat.Info} : {stat.Value}
+								</li>
 							{/if}
 						{/if}
-					</p>
-				{/each}
-			{:catch error}
-				<p style="color: red">{error.message}</p>
-			{/await}
-		</ol>
-		<p class="text-2xl text-begin uppercase; margin: 10px">Numeradores a acabar</p>
-		<ol style="margin: 10px">
-			{#each $numeradores as num}
-				<li
-					style="border: 1px solid #ddd;
-						margin-top: -1px; /* Prevent double borders */
-						background-color: #C9C0BE;
-						padding: 12px;  color: red"
-				>
-					{num.name} : {num.resto}
-				</li>
-			{/each}
-		</ol>
-	</div>
+
+						<p hidden>
+							{#if stat.Info == 'Services inactive'}
+								{#if stat.Value > 0}
+									{setServicesTrue()}
+								{/if}
+							{/if}
+						</p>
+					{/each}
+				</ol>
+				{#if $numeradores.length > 0}
+					<p class="text-2xl text-begin uppercase; margin: 10px">Numeradores a acabar</p>
+				{/if}
+				<ol style="margin: 10px">
+					{#each $numeradores as num}
+						<li
+							style="border: 1px solid #ddd; margin-top: -1px; background-color: #C9C0BE; padding: 12px;  color: red"
+						>
+							{num.name} : {num.resto}
+						</li>
+					{/each}
+				</ol>
+			</div>
+		</div>
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
 </div>
 
 <Ref />
